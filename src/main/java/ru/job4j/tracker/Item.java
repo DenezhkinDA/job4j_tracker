71dcd35f2853b2cb01fec24a368a1a2b9c020558
ДиЕ <denezhkin.dda@gmail.com>
2022-02-26 18:53:17 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -19,4 +19,31 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void printInfo() {
+        System.out.println("Номер Id: " + id);
+        System.out.println("Наименование изделия: " + name);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Item first = new Item();
+        Item second = new Item("Table");
+        Item third = new Item(155620, "Window");
+        first.printInfo();
+        second.printInfo();
+        third.printInfo();
+    }
 }
