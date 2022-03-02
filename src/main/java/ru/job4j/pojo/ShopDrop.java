c1d3f9d72b8e6c1843c9c32937941e7bb2f73632
ДиЕ <denezhkin.dda@gmail.com>
2022-03-03 00:44:38 +0300

    3.2. Удаление моделей из массива. [#310016]

---
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -0,0 +1,18 @@
+package ru.job4j.pojo;
+
+public class ShopDrop {
+    public static Product[] delete(Product[] products, int index) {
+        products[index] = null;
+        for (int i = index; i < products.length - 1; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                products[i] = products[i + 1];
+                products[i + 1] = null;
+                    System.out.println(products[i]);
+            }
+        }
+        return products;
+    }
+}
