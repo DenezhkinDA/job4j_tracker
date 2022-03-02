30fd3a36fffa0f93c596be89c7e97441694b0f82
ДиЕ <denezhkin.dda@gmail.com>
2022-03-02 22:46:28 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,33 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book pinocchio = new Book("Pinocchio", 185);
+        Book ognivo = new Book("Ognivo", 155);
+        Book konyaga = new Book("Konyaga", 177);
+        Book cleanCode = new Book("Clean Code", 77);
+        Book[] bks = new Book[4];
+        bks[0] = pinocchio;
+        bks[1] = ognivo;
+        bks[2] = konyaga;
+        bks[3] = cleanCode;
+        for (int index = 0; index < bks.length; index++) {
+            Book bk = bks[index];
+            System.out.println(bk.getName() + " - " + bk.getPages());
+        }
+        System.out.println("Swap positions 0 and 3.");
+        bks[0] = cleanCode;
+        bks[3] = pinocchio;
+        for (int index = 0; index < bks.length; index++) {
+            Book bk = bks[index];
+            System.out.println(bk.getName() + " - " + bk.getPages());
+        }
+        System.out.println("Only book Clean Code");
+        for (int index = 0; index < bks.length; index++) {
+            Book bk = bks[index];
+            if (bk.getName().equals("Clean Code")) {
+                System.out.println(bk.getName() + " - " + bk.getPages());
+            }
+        }
+    }
+}
