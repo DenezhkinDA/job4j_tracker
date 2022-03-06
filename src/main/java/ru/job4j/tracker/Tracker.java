cbff11a7e404f4d58cfe68dd1aeb6e41fe9fef17
DenezhkinDA <denezhkin.dda@gmail.com>
2022-03-06 11:56:34 +0300

    7. Метод удаления заявки Tracker.delete [#211749 #272220]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -63,7 +63,7 @@ public class Tracker {
         if (rsl) {
             return false;
         }
-        System.arraycopy(items, index + 1, items, size - 1, size - index);
+        System.arraycopy(items, index + 1, items, index, size - index - 1);
         items[size - 1] = null;
         size--;
         return true;
