20a3c10d576254c77ceed262ccdaea9dc0936eee
DenezhkinDA <denezhkin.dda@gmail.com>
2022-03-07 14:42:24 +0300

    9. toString [#310018 #272259]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -12,12 +12,6 @@ public class Item {
 
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
-    @Override
-    public String toString() {
-        return "Item{" + "id=" + id + ", name='" + name + '\''
-                + ", created=" + created.format(FORMATTER) + '}';
-    }
-
     public Item() {
     }
 
@@ -48,5 +42,11 @@ public class Item {
 
     public LocalDateTime getCreated() {
         return created;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "id=" + id + ", name='" + name + '\''
+                + ", created=" + created.format(FORMATTER) + '}';
     }
 }
